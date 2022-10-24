85fa941fc65576c496c45293c5925b26df1beffd
John Kim <johnk419@hotmail.com>
2022-10-24 14:11:21 +0900

    Adding root level API templates in generator

--- tools/generators/app-generator/index.ts
+++ tools/generators/app-generator/index.ts
@@ -1,5 +1,7 @@
 import { Tree } from '@nrwl/devkit';
-import { PrismaClientGenerator }  from '../prisma-generator'
+//import { PrismaClientGenerator }  from '../prisma-generator'
+import { joinPathFragments, updateJson } from '@nrwl/devkit';
+import { applicationGenerator as NestAppGenerator } from '@nrwl/nest';
 
 interface GeneratorOptions {
   name: string;
@@ -7,8 +9,32 @@ interface GeneratorOptions {
   connectionString: string;
 }
 
+/*
+interface ApplicationGeneratorOptions {
+  name: string;
+  directory?: string;
+  frontendProject?: string;
+  linter?: Linter;
+  skipFormat?: boolean;
+  skipPackageJson?: boolean;
+  standaloneConfig?: boolean;
+  tags?: string;
+  unitTestRunner?: UnitTestRunner;
+  setParserOptionsProject?: boolean;
+}
+*/
+
 export async function NestAPIGenerator (tree: Tree, options: GeneratorOptions) {
-  await PrismaClientGenerator(tree, {...options,});
+  //await PrismaClientGenerator(tree, {...options,});
+  updateJson(
+    tree,
+    'angular.json',
+    (json) => {
+      json.projects.${options.name} = '${options.name}'
+      return json;
+    }
+  )
+  tree.delete
 }
 
 export default NestAPIGenerator;
