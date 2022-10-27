da7a9ebb979bf35e3074d879ee8b0e6be388af90
John Kim <johnk419@hotmail.com>
2022-10-27 11:11:54 +0900

    Added PGAdmin dockerfile, continuing work on Nx Generator for API app

--- tools/generators/app-generator/index.ts
+++ tools/generators/app-generator/index.ts
@@ -1,6 +1,9 @@
 import { formatFiles, generateFiles, names, Tree } from '@nrwl/devkit';
+import { DIR_DOCUMENT } from '@angular/cdk/bidi';
 import { PrismaClientGenerator }  from '../prisma-generator'
 import { joinPathFragments, updateJson } from '@nrwl/devkit';
+import fs from 'fs'
+import * as YAML from 'yaml';
 
 interface GeneratorOptions {
   name: string;
@@ -8,6 +11,8 @@ interface GeneratorOptions {
   connectionString: string;
   port: number;
 }
+
+
 
 /*
 interface ApplicationGeneratorOptions {
@@ -26,33 +31,62 @@ interface ApplicationGeneratorOptions {
 
 export async function NestAPIGenerator (tree: Tree, options: GeneratorOptions) {
   //
-  const { name } = names(options.name)
-  const foldername = name.charAt(0).toLowerCase() + name.slice(1);
+  
+  const { name } = names(options.name);
+  const projectname = name.charAt(0).toLowerCase() + name.slice(1);
   const port = options.port;
+  const postgres_containerName = `postgres_${projectname}`;
 
+  /*
+  //Update angular.json with new API proj
   updateJson(
     tree,
     'angular.json',
     (json) => {
-      json.projects[name] = '${options.name}'
+      json.projects[projectname] = `apps/${projectname}`
       return json;
     }
   )
 
+  //Generate Nest API using template files in generator
   generateFiles(
     tree,
     joinPathFragments(__dirname, './template'),
-    `apps/${name}`,
+    `apps/`,
     {
       tmpl: '',
       name,
       port,
-      foldername
+      projectname
     }
   )
-  await formatFiles(tree)
 
   await PrismaClientGenerator(tree, {...options,});
+  */
+  let dockercompose = tree.read('docker-compose.yml')?.toString() ?? ``
+  if (dockercompose != '') {
+    const doc:YAML.Document = YAML.parseDocument(dockercompose);
+    if (doc.hasIn(['services', postgres_containerName])) {
+      doc.deleteIn(['services', postgres_containerName])
+    }
+    doc.addIn(['services'], doc.createPair(postgres_containerName, {
+      image: 'postgres:14',
+      container_name: postgres_containerName,
+      environment: {
+        POSTGRES_USER: 'zenadmin',
+        POSTGRES_PASSWORD: 'temp'
+      },
+      volumes: [doc.createPair(postgres_containerName, '/var/lib/postgresql/data')],
+      ports: [`5445:5432`]
+    }))
+    if (doc.hasIn(['volumes', postgres_containerName])) {
+      doc.deleteIn(['volumes', postgres_containerName])
+    }
+    doc.addIn(['volumes'], doc.createPair(postgres_containerName, ''));
+    console.log(doc.toString());
+  }
+  
+  await formatFiles(tree)
 }
 
 export default NestAPIGenerator;
