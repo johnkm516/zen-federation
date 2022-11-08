9d31c5d3b0503bdebcfe06fb0e44656a70c9effa
John Kim <johnk419@hotmail.com>
2022-11-08 14:11:38 +0900

    Fixed prisma select error due to multiple prisma clients and custom prisma client paths

--- apps/auth-api/src/app/prisma/prisma-select-args.ts
+++ apps/auth-api/src/app/prisma/prisma-select-args.ts
@@ -1,8 +1,10 @@
 import { PrismaSelect } from '@paljs/plugins';
 import { GraphQLResolveInfo } from 'graphql';
+import { Prisma } from '.prisma/Auth-client';
+import { DMMF } from '@prisma/client/runtime';
 
 export function PrismaSelectArgs(info: GraphQLResolveInfo, args: any) {
-  const result = new PrismaSelect(info).value;
+  const result = new PrismaSelect(info, {dmmf: [Prisma.dmmf as DMMF.Document]}).value;
 
   if (!result.select || Object.keys(result.select).length > 0) {
     return {
