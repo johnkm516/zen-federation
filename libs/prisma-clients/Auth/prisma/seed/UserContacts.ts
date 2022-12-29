58d2b8248f6c26f8e3bd2506d8ac9acb64a981d4
John Kim <johnk419@hotmail.com>
2022-12-29 10:37:44 +0900

    Added user contacts seed

--- libs/prisma-clients/Auth/prisma/seed/UserContacts.ts
+++ libs/prisma-clients/Auth/prisma/seed/UserContacts.ts
@@ -1,25 +1,26 @@
 import { PrismaClient, User } from '.prisma/Auth-client';
 
-export async function seedUsersContacts(client: PrismaClient, users : User[]) {
-    for (let i = 0; i < users.length; i++) {
-        for (let y = 0; y < users.length; y++) {
-            if (i == y) {
-                continue;
-            }
-            client.user.update({
-                data: {
-                    contacted: {
-                        connect: [
-                            {
-                                id: users[i].id
-                            }
-                        ]
-                    }
-                },
-                where: {
-                    id: users[y].id
-                }
-            })
-        }
+export async function seedUsersContacts(client: PrismaClient, users: User[]) {
+  for (let i = 0; i < users.length; i++) {
+    for (let y = 0; y < users.length; y++) {
+      if (i == y) {
+        continue;
+      }
+      
+      await client.user.update({
+        where: {
+          id: users[i].id,
+        },
+        data: {
+          contacted: {
+            connect: [
+              {
+                id: users[y].id,
+              },
+            ],
+          },
+        },
+      });
     }
+  }
 }
