4a95284a5553ce38ed939cf391e7462e37b4d810
John Kim <johnk419@hotmail.com>
2023-01-02 17:28:58 +0900

    WIP seed for CalendarsOnUsers

---
+++ libs/prisma-clients/Auth/prisma/seed/CalendarEvents.ts
@@ -0,0 +1,32 @@
+import { Calendar, PrismaClient, User, CalendarEvent } from '.prisma/Auth-client';
+
+export async function seedCalendarsOnUsers(
+  client: PrismaClient,
+  calendars: Calendar[]
+)  {
+
+    for (const calendar of calendars) {
+        switch(calendar.calendarType) {
+            case 'Personal': {
+
+                break;
+            }
+            case 'Business': {
+                
+                break;
+            }
+            case 'Family': {
+                
+                break;
+            }
+            case 'Holiday': {
+                
+                break;
+            }
+            case 'Misc': {
+                
+                break;
+            }
+        } 
+    }
+}
